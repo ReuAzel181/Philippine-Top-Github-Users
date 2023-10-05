bef73e6281f45c456940de7e8358265e4f7adef1
RAIZEL0130 <yureasel@gmail.com>
2023-10-06 01:28:21 +0800

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,40 @@
+﻿Console.Write("Please input your grade: ");
+int grade = 0;
+Boolean InvalidInput = false;
+try
+{
+    grade = Convert.ToInt32(Console.ReadLine()); 
+
+}
+catch (FormatException)
+{
+    Console.WriteLine("Invalid Input");
+    InvalidInput = true;
+}
+if (!InvalidInput) { 
+    if (grade > 100)
+    {
+        Console.WriteLine("Invalid Output!");
+    }
+    else if (grade >= 98)
+    {
+        Console.WriteLine("You are with highest honor!");
+    }
+    else if (grade >= 95)
+    {
+        Console.WriteLine("You are with high honor!");
+    }
+    else if (grade >= 90)
+    {
+        Console.WriteLine("You are with honor!");
+    }
+    else if (grade >= 75)
+    {
+        Console.WriteLine("You Passed!");
+    }
+    else
+    {
+        Console.WriteLine("You Failed!");
+    }
+
+}
